60f963817c9679cb7b25bfef1291175e3c63e853
Marcellino04 <iceltipar2005@gmail.com>
2022-07-27 15:19:48 +0700

    marcellino

---
+++ resources/views/Template/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav bg-dark">
+    <a class="nav-link active text-white" aria-current="page" href="#">MBshop</a>
+    <a class="nav-link text-white" href="#">Home</a>
+    <a class="nav-link text-white" href="#">Produk</a>
+    <a class="nav-link text-white">Keranjang</a>
+    <a class="nav-link text-white">Checkout</a>
+  </nav>
